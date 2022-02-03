fc644ac34bebca2a5631fea22c4a3c7694c15068
anumahalle <anumahalle16@gmail.com>
2022-02-03 10:55:06 +0530

    commit for practice

--- src/main/java/com/infosys/ip/InfyGoBootRestClientApplication.java
+++ src/main/java/com/infosys/ip/InfyGoBootRestClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InfyGoBootRestClientApplication {
 
 	public static void main(String[] args) {
+		System.out.println("enter");
 		SpringApplication.run(InfyGoBootRestClientApplication.class, args);
 	}
 
